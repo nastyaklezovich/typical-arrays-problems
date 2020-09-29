9ffd32d0f78d94ffb7e9c415e68fda90b8c3c9cb
Klezovich Anastasiya <nastyaklezovich@gmail.com>
2020-09-29 14:29:07 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
+exports.min = function min(array) {
+    if (array == undefined || array.length == 0) return 0;
+    let minimal = array[0];
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] <= minimal) {
+            minimal = array[i];
+        }
+    }
+    return minimal;
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (array == undefined || array.length == 0) return 0;
+    let maximum = array[0];
+    for (let i = 1; i < array.length; i++) {
+        if (array[i] >= maximum) {
+            maximum = array[i];
+        }
+    }
+    return maximum;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    let sum = 0;
+    if (array == undefined || array.length == 0) return 0;
+    for (let i = 0; i < array.length; i++) {
+        sum += array[i];
+    }
+    const avg = sum / array.length;
+    return avg;
+};
